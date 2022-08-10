b472237069fc1b215c058b97291ac50bf027bb45
Devin Richards <devin.richards@alcon.com>
2022-08-10 17:40:15 -0400

    added documentation

--- proxy-container.sh
+++ proxy-container.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+#
+# Example Usage:
+# ./proxy-container.sh traefik/whoami whoami
+
+
+#
+# Get image name and optional name of the container
+
 IMAGE=$1
 NAME=$2
+
+
+#
+# If name is not provided derrive it
 #NAME=${2-:${${1%%:*}##*/}}
 
 if [[ -z ${NAME} ]]
@@ -9,6 +21,10 @@ then
 	NAME=${IMAGE%%:*}
 	NAME=${NAME##*/}
 fi
+
+
+#
+# Run the image exposing to Traefik
 
 docker run --rm -itd --name "${NAME}" --network "proxy" --label "traefik.http.routers.${NAME}.tls=true" ${IMAGE}
 
